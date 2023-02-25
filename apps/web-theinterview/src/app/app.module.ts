f7c43fcbdc5bccdff225c8ab7dbbb03241593f4d
Rob Stadnick <robstadnick@gmail.com>
2023-02-25 07:41:24 -0700

    added NGRX for client

--- apps/web-theinterview/src/app/app.module.ts
+++ apps/web-theinterview/src/app/app.module.ts
@@ -1,14 +1,49 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 
 // app
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './features/shared/shared.module';
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { of } from 'rxjs';
+
+function appInitializer() {
+  return of(true);
+}
 
 @NgModule({
-  imports: [CoreModule, SharedModule, AppRoutingModule],
   declarations: [AppComponent],
+  imports: [
+    CoreModule,
+    SharedModule,
+    AppRoutingModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production
+      ? StoreDevtoolsModule.instrument({ maxAge: 50, logOnly: true })
+      : [],
+  ],
+  providers: [
+    // {
+    //   provide: APP_INITIALIZER,
+    //   useFactory: appInitializer,
+    //   multi: true,
+    //   // deps: [Store],
+    // },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- apps/web-theinterview/src/app/features/home/components/home.component.ts
+++ apps/web-theinterview/src/app/features/home/components/home.component.ts
@@ -5,5 +5,6 @@ import { BaseComponent } from '@theinterview/xplat/core';
 @Component({
   selector: 'theinterview-home',
   templateUrl: 'home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent extends BaseComponent {}
