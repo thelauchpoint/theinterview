3b5be46d4e3f10f7c12e73155296141a705aed2b
Rob Stadnick <robstadnick@gmail.com>
2023-03-03 12:09:00 -0700

    FE test 1 complete, patch packages

--- libs/xplat/web/features/src/lib/my-feature/my-feature.module.ts
+++ libs/xplat/web/features/src/lib/my-feature/my-feature.module.ts
@@ -5,11 +5,12 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { FeatureEffects, featureReducer, FEATURE_SERVICES, FEATURE_STATE_KEY } from '@theinterview/xplat/features';
 import { UIModule } from '../ui/ui.module';
+import { TESTFEATURE_COMPONENTS } from './components';
 
 @NgModule({
   imports: [UIModule, StoreModule.forFeature(FEATURE_STATE_KEY, featureReducer), EffectsModule.forFeature([FeatureEffects])],
   exports: [UIModule],
-  declarations: [],
+  declarations: [TESTFEATURE_COMPONENTS],
   providers: [FEATURE_SERVICES],
 })
 export class MyFeatureModule {}
