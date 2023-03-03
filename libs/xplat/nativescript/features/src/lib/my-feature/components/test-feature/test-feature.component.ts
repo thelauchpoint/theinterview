93b2b763fab1678cb5a642d52e174fbd0e0dee9f
Brian Clow <brianrclow@gmail.com>
2023-03-03 15:22:04 -0700

    add to feature screen

--- libs/xplat/nativescript/features/src/lib/my-feature/components/test-feature/test-feature.component.ts
+++ libs/xplat/nativescript/features/src/lib/my-feature/components/test-feature/test-feature.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
-
 import { TestFeatureBaseComponent } from '@theinterview/xplat/features';
 
 @Component({
   selector: 'theinterview-test-feature',
   templateUrl: 'test-feature.component.html',
 })
-export class TestFeatureComponent extends TestFeatureBaseComponent {
-  constructor(_Store: Store) {
-    super(_Store);
-  }
-}
+export class TestFeatureComponent extends TestFeatureBaseComponent {}
