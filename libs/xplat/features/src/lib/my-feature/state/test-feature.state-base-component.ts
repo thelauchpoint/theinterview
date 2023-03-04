fd4277c0e0d0d5cb28425b8660788b2c1eae8a14
Kasey McCormick <mccormick.kasey@gmail.com>
2023-03-03 17:29:11 -0700

    fe extra component added

--- libs/xplat/features/src/lib/my-feature/state/test-feature.state-base-component.ts
+++ libs/xplat/features/src/lib/my-feature/state/test-feature.state-base-component.ts
@@ -1,6 +1,5 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { IFeature } from '@theinterview/core-types';
 import { BaseComponent } from '@theinterview/xplat/core';
 import { Observable } from 'rxjs';
 import { selectFeatureError, selectSelectedFeature } from './feature.selector';
@@ -11,7 +10,7 @@ export abstract class TestFeatureStateBaseComponent extends BaseComponent {
   error$: Observable<{ statusCode?: string; error: string; message: string } | null>;
   feature$: Observable<FeatureEnitityState | undefined>;
   // featureLabels$: Observable<{ label: string; key: string }[]>;
-
+  
   constructor(public _Store: Store) {
     super();
     this.error$ = this._Store.select(selectFeatureError);
