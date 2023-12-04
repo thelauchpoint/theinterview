059a4f665068310478851d63be01c18b7c0addf4
Your NameCourtney <cmacarson@gmail.com>
2023-12-04 14:58:59 -0800

    component immediately shows the days value from the input

--- apps/web-theinterview/src/app/features/components/components.component.ts
+++ apps/web-theinterview/src/app/features/components/components.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { BaseComponent } from '@theinterview/xplat/core';
+import { CardInputs } from './court-day-step/court-day-step.component';
 
 @Component({
   selector: 'theinterview-components',
   templateUrl: 'components.component.html',
 })
 export class ComponentsComponent extends BaseComponent {
+  inputs: Array<CardInputs> = [];
+
   constructor() {
     super();
+    this.inputs = [
+      {
+        title: 'courtney',
+        disabled: false,
+        canProceed: true,
+        days: 36,
+      },
+    ];
   }
 }
--- apps/web-theinterview/src/app/features/components/court-day-step/court-day-step.component.ts
+++ apps/web-theinterview/src/app/features/components/court-day-step/court-day-step.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-interface CardInputs {
+export interface CardInputs {
   title: string;
   disabled: boolean;
   canProceed: boolean;
@@ -22,13 +22,26 @@ export class CourtDayStepComponent {
   days: number | undefined;
 
   @Output() disabledChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
-  @Output() daySelected: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() daysSelected: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  // need to get the logic when the page loads not when it's clicked and I think that I already have it on init so lets try to pass it in
+
+  ngOnInit() {
+    console.log('cardInputs', this.inputs);
+    this.inputs.forEach((cardInput: CardInputs) => {
+      // Your logic here
+      this.daySelected(cardInput);
+      console.log('inside for each', cardInput)
+    });
+  }
 
   // logic for how many to show - if days = 36 then show 36, if days = null then use index + 1
   // if disabled is set to null then all would be enabled.
   // if disabled is set to true then all would be disabled.
-  howManyDaysToShow(cardInputs: CardInputs, index: number) {
+  // daySelected(cardInputs: CardInputs, index: number) {
+  daySelected(cardInputs: CardInputs) {
     // would I just use the length here instead of the index?
+    const index = this.inputs.length;
     if (cardInputs.days === null) {
       // return
       this.days = index + 1;
@@ -44,13 +57,12 @@ export class CourtDayStepComponent {
   }
 }
 
-
 // todo:
-// inputs array object (1 item is 1 card)
-// ouputs for which one is selected (day)
+// inputs array object (1 item is 1 card) ✅
+// ouputs for which one is selected (day) 
 // output for one selected if disabled
-// array length as default value for how many would go in
-// default value for index +1, or if they input 36 then show 36
+// array length as default value for how many would go in ✅
+// default value for index +1, or if they input 36 then show 36 ✅
 // always a top bar - so horizontal scroll needed
 // if disable is set to null then all would be enabled.
 
