e4fef11afc36e7245fee9359d578fa6595f08c1e
Your NameCourtney <cmacarson@gmail.com>
2023-12-05 13:00:52 -0800

    .

--- apps/web-theinterview/src/app/features/components/components.component.ts
+++ apps/web-theinterview/src/app/features/components/components.component.ts
@@ -18,6 +18,36 @@ export class ComponentsComponent extends BaseComponent {
         canProceed: true,
         days: 36,
       },
+      {
+        title: 'asdf',
+        disabled: false,
+        canProceed: true,
+        days: 6,
+      },
+      {
+        title: 'courtnesdghjy',
+        disabled: false,
+        canProceed: true,
+        days: 16,
+      },
+      {
+        title: 'couhdgjtyjtyrtney',
+        disabled: false,
+        canProceed: true,
+        days: 8,
+      },
+      {
+        title: 'courfghsfgtney',
+        disabled: false,
+        canProceed: true,
+        days: 4,
+      },
+      {
+        title: 'cour2355tney',
+        disabled: true,
+        canProceed: true,
+        days: 0,
+      },
     ];
   }
 }
