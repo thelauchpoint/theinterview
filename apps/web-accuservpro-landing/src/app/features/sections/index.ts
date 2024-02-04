808f3f71043fb060050ceaed951f97fca695bd50
Your NameCourtney <cmacarson@gmail.com>
2024-02-04 14:26:48 -0800

    messin with tailwind

--- apps/web-accuservpro-landing/src/app/features/sections/index.ts
+++ apps/web-accuservpro-landing/src/app/features/sections/index.ts
@@ -1,4 +1,6 @@
+import { sectionHeroComponent } from './hero/section-hero.component';
 import { quote01Component } from './quote-01/quote-01.component';
 
-export const LANDING_SECTION_COMPONENTS = [quote01Component];
+export const LANDING_SECTION_COMPONENTS = [quote01Component, sectionHeroComponent];
 export * from './quote-01/quote-01.component';
+export * from './hero/section-hero.component';
