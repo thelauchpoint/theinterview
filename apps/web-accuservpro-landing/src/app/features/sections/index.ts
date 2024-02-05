0ef9a78205be17b99be039f46359cae8aacbaf82
Your NameCourtney <cmacarson@gmail.com>
2024-02-04 16:47:38 -0800

    prelaunch and quotes section in

--- apps/web-accuservpro-landing/src/app/features/sections/index.ts
+++ apps/web-accuservpro-landing/src/app/features/sections/index.ts
@@ -1,6 +1,8 @@
 import { sectionHeroComponent } from './hero/section-hero.component';
+import { preLaunchSlotsComponent } from './prelaunch-slots/prelaunch-slots.component';
 import { quote01Component } from './quote-01/quote-01.component';
 
-export const LANDING_SECTION_COMPONENTS = [quote01Component, sectionHeroComponent];
-export * from './quote-01/quote-01.component';
+export const LANDING_SECTION_COMPONENTS = [quote01Component, sectionHeroComponent, preLaunchSlotsComponent];
 export * from './hero/section-hero.component';
+export * from './prelaunch-slots/prelaunch-slots.component';
+export * from './quote-01/quote-01.component';
